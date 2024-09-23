5984cd0c407f7214167e01aea5fb70e42fbe4b0a
ITITIU20292 <ITITIU20292@student.hcmiu.edu.vn>
2024-09-23 17:51:16 +0700

    Model User

---
+++ src/main/java/org/sang/labmanagement/user/User.java
@@ -0,0 +1,128 @@
+package org.sang.labmanagement.user;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import java.security.Principal;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import javax.security.auth.Subject;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.sang.labmanagement.security.email.EmailVerificationCode;
+import org.sang.labmanagement.security.token.Token;
+import org.sang.labmanagement.user.instructor.Instructor;
+import org.sang.labmanagement.user.student.Student;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class User implements UserDetails , Principal {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String firstName;
+
+	private String lastName;
+
+	private String username;
+
+	private String password;
+
+	private String phoneNumber;
+
+
+	@Column(unique = true)
+	private String email;
+
+	@Enumerated(EnumType.STRING)
+	private Role role;
+
+	private boolean accountLocked;
+
+	private boolean enabled;
+
+	@OneToOne(mappedBy = "user",cascade = CascadeType.ALL)
+	private Student student;
+
+	@JsonBackReference
+	@OneToOne(mappedBy = "user",cascade = CascadeType.ALL)
+	private Instructor instructor;
+
+	@CreatedDate
+	@Column(nullable = false,updatable = false)
+	private LocalDateTime createdDate;
+
+	@LastModifiedDate
+	@Column(insertable = false)
+	private LocalDateTime lastModifiedDate;
+
+	@OneToMany(mappedBy = "user")
+	private List<Token> token;
+
+	@OneToMany(mappedBy = "user")
+	private List<EmailVerificationCode> emailCode;
+
+	public String getFullName(){
+		return lastName+" "+firstName;
+	}
+
+	@Override
+	public String getName() {
+		return username;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority("ROLE_"+role.name()));
+	}
+
+	@Override
+	public boolean implies(Subject subject) {
+		return Principal.super.implies(subject);
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return !accountLocked;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+}
