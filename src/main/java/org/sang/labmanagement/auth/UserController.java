a4b73dc1d34a3254857361a5cf9d8a4b98f033d5
ITITIU20292 <ITITIU20292@student.hcmiu.edu.vn>
2024-10-23 00:03:15 +0700

    User profile and logout

---
+++ src/main/java/org/sang/labmanagement/auth/UserController.java
@@ -0,0 +1,25 @@
+package org.sang.labmanagement.auth;
+
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import org.sang.labmanagement.user.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+	private final AuthenticationService authService;
+
+	@GetMapping("/profile")
+	public ResponseEntity<User> findUserByJwt(Authentication connectedUser) {
+		User user = authService.findUser(connectedUser);
+		return ResponseEntity.ok(user);
+	}
+
+}
