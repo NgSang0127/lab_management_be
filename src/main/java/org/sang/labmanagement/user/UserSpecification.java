7a6a8dd1c193a9b35aadb2e9cd54111987045446
ITITIU20292 <ITITIU20292@student.hcmiu.edu.vn>
2025-01-17 18:34:48 +0700

    User Activity Event

---
+++ src/main/java/org/sang/labmanagement/user/UserSpecification.java
@@ -0,0 +1,30 @@
+package org.sang.labmanagement.user;
+
+import jakarta.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.jpa.domain.Specification;
+
+public class UserSpecification {
+
+	public static Specification<User> getUsersByKeywordAndRole(String keyword, String role) {
+		return (root, query, criteriaBuilder) -> {
+			List<Predicate> predicates = new ArrayList<>();
+
+			if (keyword != null && !keyword.isEmpty()) {
+				String pattern = "%" + keyword.toLowerCase() + "%";
+				Predicate firstNamePredicate = criteriaBuilder.like(criteriaBuilder.lower(root.get("firstName")), pattern);
+				Predicate lastNamePredicate = criteriaBuilder.like(criteriaBuilder.lower(root.get("lastName")), pattern);
+				Predicate usernamePredicate = criteriaBuilder.like(criteriaBuilder.lower(root.get("username")), pattern);
+				predicates.add(criteriaBuilder.or(firstNamePredicate, lastNamePredicate, usernamePredicate));
+			}
+
+			if (role != null && !role.isEmpty()) {
+				predicates.add(criteriaBuilder.equal(root.get("role"), role));
+			}
+
+			return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+		};
+	}
+}
